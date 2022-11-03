fb625e71081d1233080eeb3c1262db0a9a11bb4b
Cesar Hernandez <cesar.lopex@gmail.com>
2022-11-02 20:13:34 -0600

    estados simples

--- src/ClassState.js
+++ src/ClassState.js
@@ -2,13 +2,30 @@ import { render } from "@testing-library/react";
 import React from "react";
 
 class ClassState extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            error: true,
+        };
+    }
+
     render () {
+        
         return (
             <div>
-                <h2>Eliminar ClassState</h2>
+                <h2>Eliminar ClassState{this.props.name}</h2>
                 <p>por favor escribe el codigo de seguridad </p>
+                
+                {this.state.error && (
+                <p>error: el codigo es incorrecto</p>
+                )}
+
                 <input placeholder="codigo de seguridad"/>
-                <button>Comprobar</button>
+                <button
+                    onClick={()=>
+                       this.setState(prevState => ({ error: !prevState.error }))
+                    }
+                >Comprobar</button>
             </div>
         )
     }
--- src/UseState.js
+++ src/UseState.js
@@ -1,12 +1,18 @@
 import React from "react";
 
 function UseState() {
+    const [error, setError]= React.useState(true);
     return (
         <div>
             <h2>Eliminar UseState</h2>
             <p>por favor escribe el codigo de seguridad </p>
+            {error && (
+                <p>error: el codigo es incorrecto</p>
+            )}
             <input placeholder="codigo de seguridad"/>
-            <button>Comprobar</button>
+            <button
+                onClick={() => setError(!error) }
+            >Comprobar</button>
         </div>
     )
 }
